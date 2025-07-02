63029f5a8362877293962092b8b74657c8ca8bc0
Gregory Randolph <gloaming@gmail.com>
2025-07-02 14:15:00 -0700

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Gregory Randolph on 7/2/25.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
